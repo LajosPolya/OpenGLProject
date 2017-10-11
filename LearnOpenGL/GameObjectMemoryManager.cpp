55f454df3cce672d732c774ddd5f4f4557554c25
LajosPolya <lajospolya94@gmail.com>
2017-10-10 22:24:44 -0400

    Remove functionality for GameObjectMemoryManager

--- LearnOpenGL/GameObjectMemoryManager.cpp
+++ LearnOpenGL/GameObjectMemoryManager.cpp
@@ -3,14 +3,9 @@
 /* static member must be defiend in the .cpp file */
 std::map<void *, int> GameObjectMemoryManager::manager;
 
-GameObjectMemoryManager::GameObjectMemoryManager()
-{
-}
+GameObjectMemoryManager::GameObjectMemoryManager() {}
 
-
-GameObjectMemoryManager::~GameObjectMemoryManager()
-{
-}
+GameObjectMemoryManager::~GameObjectMemoryManager() {}
 
 void GameObjectMemoryManager::add(void * ptr)
 {
@@ -21,5 +16,18 @@ void GameObjectMemoryManager::add(void * ptr)
 	}
 	else {
 		manager[ptr] = ++it->second;
+	}
+}
+
+int GameObjectMemoryManager::decrement(void * ptr)
+{
+	std::map<void *, int>::iterator it = manager.find(ptr);
+
+	if (it == manager.end()) {
+		return -1;
+	}
+	else {
+		manager[ptr] = --it->second;
+		return it->second;
 	}
 }
--- LearnOpenGL/GameObjectMemoryManager.h
+++ LearnOpenGL/GameObjectMemoryManager.h
@@ -9,6 +9,7 @@ public:
 	~GameObjectMemoryManager();
 
 	static void add(void * ptr);
+	static int decrement(void * ptr);
 
 private:
 	static std::map<void *, int> manager;
